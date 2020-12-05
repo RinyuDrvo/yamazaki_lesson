e0c092fcbe38b55e3f41f20d89a398b56a363db9
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-12-05 18:11:04 +0900

    q2のテスト作成

--- src/q2.js
+++ src/q2.js
@@ -1,2 +1,13 @@
-const b = 5;
+/**
+ * 数値か文字列を受け取る引数を1つ取り、
+ * それが文字列であった場合は「文字列」、
+ * 数値であった場合は「数値」という文字列を出力する関数を作成してください。
+ */
 
+function q2(arg) {
+  /** 回答を記述 */
+  if(typeof arg === 'string') return '文字列';
+  if(typeof arg === 'number') return '数値';
+}
+
+module.exports = q2;
