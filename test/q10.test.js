f56e56dfdbbe3dfa2fc870456b6d4a076b506e7f
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-12-17 10:37:41 +0900

    q10テスト作成

--- test/q10.test.js
+++ test/q10.test.js
@@ -8,3 +8,11 @@ test("add 'javascript' in ['a', 'b', 'c', 'd'] and 2 is equal ['a', 'b', 'javasc
   const ans = ['a', 'b', 'javascript', 'c', 'd']; 
   expect(q10(arr, str, num)).toEqual(ans);
 })
+
+test("add 'node' in ['a', 'b', 'c', 'd'] and 0 is equal ['node', 'a', 'b', 'c', 'd']", () => {
+  const arr = ['a', 'b', 'c', 'd'] ;
+  const str = 'node'
+  const num = 0;
+  const ans = ['node', 'a', 'b', 'c', 'd']; 
+  expect(q10(arr, str, num)).toEqual(ans);
+})
