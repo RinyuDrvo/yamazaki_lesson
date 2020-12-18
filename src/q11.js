45c125ba8f8bc5b8fa2979ec7030d337c7ef1b3a
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-12-18 09:54:01 +0900

    q11のテストを作成

---
+++ src/q11.js
@@ -0,0 +1,15 @@
+/**
+ * 練習課題11
+ * 第一引数に整数、第二引数に文字列をとり、{age: 第一引数, name: 第二引数}というオブジェクトを返す関数を作成してください。
+ * 例）第一引数：26 第二引数：rinyu 返り値：{age: 26, name: rinyu}
+ */
+
+function q11(age, name) {
+  /** ここに回答を記述 */
+  return {
+    age: age,
+    name: name
+  }
+}
+
+module.exports = q11;
