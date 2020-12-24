72f5266a2d3b004996fe2acb572925f8353a42ba
RinyuDrvo <alchemist.drumvocal@gmail.com>
2020-12-24 10:21:09 +0900

    未完成

--- src/q13.js
+++ src/q13.js
@@ -3,8 +3,8 @@
  * 第一引数に文字列のみを含む二次元配列、第二引数に文字列を取り、
  * 第一引数の二次元配列の中から第二引数に指定された文字列を検索し、
  * 当てはまる全てのインデックスを配列で返す関数を作成してください
- * 例）第一引数：[['abc', 'def'], ['hijk', 'lm']] 第二引数： 'ij'
- * 　　返り値：[1, 1]
+ * 例）第一引数：[['abc', 'deijf'], ['hijk', 'lm']] 第二引数： 'ij'
+ * 　　返り値：[[0, 1], [1, 0]]
  * 制限事項：文字列は必ず二次元目の配列に格納されていることとする
  */
 
@@ -19,7 +19,7 @@ function q13(arg1, arg2) {
     isAnsIndex ? ansChildIndex = childIndex : false;
     return isAnsIndex;
   });
-  return [parentIndex,ansChildIndex];
+  return [[parentIndex,ansChildIndex]];
 }
 
 module.exports = q13;
--- test/q13.test.js
+++ test/q13.test.js
@@ -1,15 +1,15 @@
 const q13 = require('../src/q13');
 
-test("adds [['abc', 'def'], ['hijk', 'lm']] and 'ij' to Equal[2, 1]", () => {
+test("adds [['abc', 'def'], ['hijk', 'lm']] and 'ij' to Equal[[2, 1]]", () => {
   const arg1 = [['abc', 'def'], ['hijk', 'lm']];
   const arg2 = 'ij';
-  const ans = [1, 0];
+  const ans = [[1, 0]];
   expect(q13(arg1, arg2)).toEqual(ans);
 })
 
-test("adds [['op', '', 'q'], ['rstuv', 'wx'], ['yz', 'abcdef', 'ghi']] and 'b' to Equal[3, 2]", () => {
-  const arg1 = [['op', '', 'q'], ['rstuv', 'wx'], ['yz', 'abcdef', 'ghi']];
-  const arg2 = 'b';
-  const ans = [2, 1];
+test("adds [['op', '', 'q'], ['rstuv', 'wx'], ['yz', 'abcdef', 'gophi']] and 'op' to Equal[[0, 0], [2, 2]]", () => {
+  const arg1 = [['op', '', 'q'], ['rstuv', 'wx'], ['yz', 'abcdef', 'gophi']];
+  const arg2 = 'op';
+  const ans = [[0, 0], [2, 2]];
   expect(q13(arg1, arg2)).toEqual(ans);
 })
