9a9ca3b003b87712cf814c392ff9f5ce825e3569
RinyuDrvo <alchemist.drumvocal@gmail.com>
2021-01-03 15:36:46 +0900

    q13作成

--- src/q13.js
+++ src/q13.js
@@ -1,25 +1,16 @@
 /**
  * 練習課題13
- * 第一引数に文字列のみを含む二次元配列、第二引数に文字列を取り、
- * 第一引数の二次元配列の中から第二引数に指定された文字列を検索し、
- * 当てはまる全てのインデックスを配列で返す関数を作成してください
- * 例）第一引数：[['abc', 'deijf'], ['hijk', 'lm']] 第二引数： 'ij'
- * 　　返り値：[[0, 1], [1, 0]]
- * 制限事項：文字列は必ず二次元目の配列に格納されていることとする
+ * 第一引数に文字列が格納された二次元配列を、第二引数に整数のインデックスを配列で受け取り、
+ * 第二引数のインデックスに示された文字列を返す関数を作成してください
+ * 例）第一引数：[['one', 'hoge'],['two', 'fuga'],['three', 'piyo@piyo']]
+ * 　　第二引数：[1, 1]
+ * 　　返り値　：'fuga'
+ * 制限事項：第一引数の二次元配列には文字列のみ渡される
  */
 
 function q13(arg1, arg2) {
   /** ここに回答を記述 */
-  let ansChildIndex = 0;
-  const parentIndex = arg1.findIndex(parentElement => {
-    const childIndex = parentElement.findIndex(childElement => {
-      return childElement.includes(arg2)
-    });
-    const isAnsIndex = childIndex !== -1;
-    isAnsIndex ? ansChildIndex = childIndex : false;
-    return isAnsIndex;
-  });
-  return [[parentIndex,ansChildIndex]];
+  return arg1[arg2[0]][arg2[1]];
 }
 
 module.exports = q13;
--- test/q13.test.js
+++ test/q13.test.js
@@ -1,15 +1,15 @@
 const q13 = require('../src/q13');
 
-test("adds [['abc', 'def'], ['hijk', 'lm']] and 'ij' to Equal[[2, 1]]", () => {
-  const arg1 = [['abc', 'def'], ['hijk', 'lm']];
-  const arg2 = 'ij';
-  const ans = [[1, 0]];
-  expect(q13(arg1, arg2)).toEqual(ans);
+test("adds [['one', 'hoge'],['two', 'fuga'],['three', 'piyo@piyo']] and [1, 1] to equal 'fuga'", () => {
+  const arg1 = [['one', 'hoge'],['two', 'fuga'],['three', 'piyo@piyo']];
+  const arg2 = [1, 1];
+  const ans = 'fuga';
+  expect(q13(arg1, arg2)).toBe(ans);
 })
 
-test("adds [['op', '', 'q'], ['rstuv', 'wx'], ['yz', 'abcdef', 'gophi']] and 'op' to Equal[[0, 0], [2, 2]]", () => {
-  const arg1 = [['op', '', 'q'], ['rstuv', 'wx'], ['yz', 'abcdef', 'gophi']];
-  const arg2 = 'op';
-  const ans = [[0, 0], [2, 2]];
-  expect(q13(arg1, arg2)).toEqual(ans);
+test("adds [['one', 'hoge', 'two'],['fuga', 'three', 'piyo@piyo']] and [0, 2] to equal 'two'", () => {
+  const arg1 = [['one', 'hoge', 'two'],['fuga', 'three', 'piyo@piyo']];
+  const arg2 = [0, 2];
+  const ans = 'two';
+  expect(q13(arg1, arg2)).toBe(ans);
 })
